8a219b8d15ed4db47772d539933e4625ca2cfe39
bellom <bellomsean@gmail.com>
2019-10-30 13:33:48 +0100

    added 4 components in src/com

--- src/component/App.js
+++ src/component/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
 
+
 function App() {
   return (
     <div className="App">
-      <h1>Calculator With React</h1>
-      <h3>Project Set Up</h3>
-      <h4>Deploy to Heroku !!></h4>
+      <Display />
+      <Button />
+      <ButtonPanel />
     </div>
   );
 }
