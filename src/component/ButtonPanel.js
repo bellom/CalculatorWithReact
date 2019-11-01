18ce3e0816060b3a00e74dfec54e24041e8792da
bellom <bellomsean@gmail.com>
2019-11-01 01:47:41 +0100

    updated style to fix stickler error

--- src/component/ButtonPanel.js
+++ src/component/ButtonPanel.js
@@ -7,34 +7,34 @@ class ButtonPanel extends React.Component {
   render() {
     return (
       <div>
-        <div className="Button">
+        <div className="Button row">
           <Button buttonName="AC" color='grey'/>
           <Button buttonName="+/-" color='grey' />
           <Button buttonName="%" color='grey' />
           <Button buttonName="÷" />
         </div>
 
-        <div className="Button">
+        <div className="Button row">
           <Button buttonName="7" color='grey' />
           <Button buttonName="8" color='grey' />
           <Button buttonName="9" color='grey' />
           <Button buttonName="x" />
         </div>
 
-        <div className="Button">
+        <div className="Button row">
           <Button buttonName="4" color='grey' />
           <Button buttonName="5" color='grey' />
           <Button buttonName="6" color='grey' />
           <Button buttonName="-" />
         </div>
 
-        <div className="Button">
+        <div className="Button row">
           <Button buttonName="1" color='grey' />
           <Button buttonName="2" color='grey' />
           <Button buttonName="3" color='grey' />
           <Button buttonName="+" />
         </div>
-        <div className="Button">
+        <div className="Button row">
           <Button buttonName="0" color='grey' wide />
           <Button buttonName="." color='grey'/>
           <Button buttonName="=" />
